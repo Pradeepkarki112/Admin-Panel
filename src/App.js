970e0e4c5a596cdb9b8727df60cddbea4c7a6f29
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-05-26 10:51:01 +0545

    getting started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Income from "./component/Income";
+import Notes from "./component/Notes";
+import Sidebar from "./component/Sidebar";
+import Topbar from "./component/Topbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sidebar />
+      <Topbar />
+      <Notes />
+      <Income />
     </div>
   );
 }
